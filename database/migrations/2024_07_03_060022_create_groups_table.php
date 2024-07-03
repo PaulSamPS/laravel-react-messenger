efce90cab6aca31300efff30e5ffe2ee23eea205
Павел Самойленко <spv@vt2b.ru>
2024-07-03 12:39:11 +0500

    seeds

--- database/migrations/2024_07_03_060022_create_groups_table.php
+++ database/migrations/2024_07_03_060022_create_groups_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
         Schema::create('group_users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('group_id')->constrained('group')->onDelete('cascade');
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
